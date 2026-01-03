8f981bf780c52dcbae0e9b267d868d0c63dca26b
Kojib <123880127+kojibai@users.noreply.github.com>
2026-01-02 21:25:51 -0500

    Route sigil links to SigilPage

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { registerSW } from 'virtual:pwa-register';
 import { SigilMarketsShell } from './SigilMarkets/SigilMarketsShell';
@@ -12,11 +12,19 @@ const queryClient = new QueryClient();
 
 registerSW({ immediate: true });
 
+const SigilPage = React.lazy(() => import('./pages/SigilPage/SigilPage'));
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <SigilMarketsShell />
+        <Suspense fallback={null}>
+          <Routes>
+            <Route path="s" element={<SigilPage />} />
+            <Route path="s/:hash" element={<SigilPage />} />
+            <Route path="*" element={<SigilMarketsShell />} />
+          </Routes>
+        </Suspense>
       </BrowserRouter>
     </QueryClientProvider>
   </React.StrictMode>
