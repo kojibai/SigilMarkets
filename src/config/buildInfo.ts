2210cc2cc51cb855969bb5bf317af719a8b49347
Kojib <kojibchat@gmail.com>
2026-01-05 16:00:34 -0500

    Update buildInfo.ts

--- src/config/buildInfo.ts
+++ src/config/buildInfo.ts
@@ -1,5 +1,5 @@
 export const APP_NAME = "Verahai";
-export const APP_VERSION = "2.0.1";
+export const APP_VERSION = "2.0.2";
 export const GITHUB_REPO_URL = "https://github.com/phinetwork/verahai";
 
 export const GITHUB_RELEASE_URL = (version: string): string =>
